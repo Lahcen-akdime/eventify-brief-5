f2ce04692ff72937893bae9ef90ec6ed723ce0c8
Lahcen <akdiml309@gmail.com>
2025-11-06 21:21:38 +0100

    KAN-8 #done Le formulaire et s'affiche ,et je peux ajouter les evennement

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 let sideBtn = document.querySelectorAll(".sidebar__btn");
-
+let events = [];
+let archive = [];
+let screens = document.querySelectorAll("section[data-screen]");
 
 sideBtn.forEach(element => {
     element.addEventListener('click', (e)=> {
-           sideBtn.forEach(btn => btn.classList.remove('is-active'))
-            element.classList.add('is-active')
-    })
-}
-)
+
+    sideBtn.forEach(btn => btn.classList.remove('is-active'))
+    element.classList.add('is-active')
+
+    screens.forEach(screen => screen.classList.toggle('is-visible', screen.dataset.screen === element.dataset.screen))
+    })   
+})
+let myform = document.getElementsByTagName("form")[0] ;
+console.log(myform)
+myform.addEventListener("submit" , (e)=> {
+e.preventDefault();
+let eventtitle = document.getElementById("event-title").value;
+console.log(eventtitle);
+let ImageURL = document.getElementById("event-image").value;
+console.log(ImageURL);
+let eventdescription = document.getElementById("event-description").value;
+console.log(eventdescription);
+let eventseats = document.getElementById("event-seats").value;
+console.log(eventseats);
+let eventprice = document.getElementById("event-price").value;
+console.log(eventprice);
+
+})
 
 
 
 
-// if(element.dataset.screen === Selection.datacet.screen)
+
+
+    
